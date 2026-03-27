c289febf9fe273950e1974572410192e873b40b4
s1azzy-codex <slazzy.main+codex@gmail.com>
2026-03-27 20:44:06 +0800

    chore: refresh outdated dependencies again (#42)

--- spec/lib/maplibre_version_check_spec.rb
+++ spec/lib/maplibre_version_check_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe MaplibreVersionCheck do
       contents = <<~TEXT
         /**
          * MapLibre GL JS
-         * @license 3-Clause BSD. Full text of license: https://github.com/maplibre/maplibre-gl-js/blob/v5.21.0/LICENSE.txt
+         * @license 3-Clause BSD. Full text of license: https://github.com/maplibre/maplibre-gl-js/blob/v5.21.1/LICENSE.txt
          */
       TEXT
 
-      expect(described_class.parse_version(contents)).to eq("5.21.0")
+      expect(described_class.parse_version(contents)).to eq("5.21.1")
     end
 
     it "raises when the version header is missing" do
@@ -23,9 +23,9 @@ RSpec.describe MaplibreVersionCheck do
 
   describe ".parse_registry_version" do
     it "extracts the latest version from npm registry metadata" do
-      body = { version: "5.21.0" }.to_json
+      body = { version: "5.21.1" }.to_json
 
-      expect(described_class.parse_registry_version(body)).to eq("5.21.0")
+      expect(described_class.parse_registry_version(body)).to eq("5.21.1")
     end
 
     it "raises when the registry payload is malformed" do
@@ -42,7 +42,7 @@ RSpec.describe MaplibreVersionCheck do
       <<~TEXT
         /**
          * MapLibre GL JS
-         * @license 3-Clause BSD. Full text of license: https://github.com/maplibre/maplibre-gl-js/blob/v5.21.0/LICENSE.txt
+         * @license 3-Clause BSD. Full text of license: https://github.com/maplibre/maplibre-gl-js/blob/v5.21.1/LICENSE.txt
          */
       TEXT
     end
@@ -58,12 +58,12 @@ RSpec.describe MaplibreVersionCheck do
     it "returns the local and latest versions when they match" do
       result = described_class.call(
         local_asset_path: asset_path,
-        latest_version_fetcher: -> { "5.21.0" }
+        latest_version_fetcher: -> { "5.21.1" }
       )
 
       expect(result).to eq(
-        local_version: "5.21.0",
-        latest_version: "5.21.0",
+        local_version: "5.21.1",
+        latest_version: "5.21.1",
         up_to_date: true
       )
     end
@@ -71,12 +71,12 @@ RSpec.describe MaplibreVersionCheck do
     it "returns an outdated result when a newer upstream version exists" do
       result = described_class.call(
         local_asset_path: asset_path,
-        latest_version_fetcher: -> { "5.21.1" }
+        latest_version_fetcher: -> { "5.21.2" }
       )
 
       expect(result).to eq(
-        local_version: "5.21.0",
-        latest_version: "5.21.1",
+        local_version: "5.21.1",
+        latest_version: "5.21.2",
         up_to_date: false
       )
     end
@@ -85,14 +85,14 @@ RSpec.describe MaplibreVersionCheck do
       expect do
         described_class.call(
           local_asset_path: Rails.root.join("tmp/missing-maplibre.js"),
-          latest_version_fetcher: -> { "5.21.0" }
+          latest_version_fetcher: -> { "5.21.1" }
         )
       end.to raise_error(MaplibreVersionCheck::Error, /Unable to read vendored maplibre-gl asset/)
     end
   end
 
   describe ".fetch_latest_version" do
-    let(:success_response) { instance_double(Net::HTTPSuccess, body: { version: "5.21.0" }.to_json) }
+    let(:success_response) { instance_double(Net::HTTPSuccess, body: { version: "5.21.1" }.to_json) }
 
     before do
       allow(success_response).to receive(:is_a?).with(Net::HTTPSuccess).and_return(true)
@@ -101,7 +101,7 @@ RSpec.describe MaplibreVersionCheck do
     it "returns the latest version from the registry response" do
       allow(Net::HTTP).to receive(:get_response).and_return(success_response)
 
-      expect(described_class.fetch_latest_version).to eq("5.21.0")
+      expect(described_class.fetch_latest_version).to eq("5.21.1")
     end
 
     it "raises a wrapped error when the registry request fails" do
@@ -127,13 +127,12 @@ RSpec.describe MaplibreVersionCheck do
     let(:stderr) { StringIO.new }
     let(:result) do
       {
-        local_version: "5.21.0",
-        latest_version: "5.21.0",
+        local_version: "5.21.1",
+        latest_version: "5.21.1",
         up_to_date: true
       }
     end
     let(:checker) { -> { result } }
-
 
     it "prints success output and returns zero when current" do
       expect(exit_code).to eq(0)
@@ -144,8 +143,8 @@ RSpec.describe MaplibreVersionCheck do
     context "when the vendored version is outdated" do
       let(:result) do
         {
-          local_version: "5.21.0",
-          latest_version: "5.21.0",
+          local_version: "5.21.1",
+          latest_version: "5.21.2",
           up_to_date: false
         }
       end
